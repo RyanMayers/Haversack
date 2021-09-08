d20c5d4def4d8b4353b147f8914d5a6af8788034
Ryan Mayers <lennyoliy@gmail.com>
2021-09-08 16:54:20 -0400

    it kind of works now

--- HaversackTest.py
+++ HaversackTest.py
@@ -1,12 +1,42 @@
-import Spell
+from Spell import *
 
 spellbook = {}
 
-def manAddSpell(book = spellbook, name):
-    prompts = ["level", "dndClass", "castingTime", "spellRange", "components", "duration", "desc"]
+def manAddSpell(book):
+    prompts = ["name", "school", "level", "dndClass", "castingTime", "spellRange", "components", "duration", "desc"]
     newSpell = {}
     for i in prompts:
         print(i + "?")
         newSpell[i] = input()
-    spellbook[name] = Spell(newSpell["level"], newSpell["dndClass"], newSpell["castingTime"], newSpell["spellRange"], newSpell["components"], newSpell["duration"], newSpell["desc"])
+    book[newSpell["name"]] = Spell(newSpell["name"], newSpell["school"], newSpell["level"], newSpell["dndClass"], newSpell["castingTime"], newSpell["spellRange"], newSpell["components"], newSpell["duration"], newSpell["desc"])
 
+        # self.name = name
+        # self.school = school
+        # self.dclass = dndClass
+        # self.level = level
+        # self.time = castingTime
+        # self.range = spellRange
+        # self.comp = components
+        # self.dur = duration
+        # self.desc = desc
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+def printSpell (spell):
+    print(color.BOLD + color.RED + spell.name + color.END)
+    print(color.UNDERLINE + "Level-" + spell.level + " " + spell.school + color.END + "\n")
+    print("Casting Time: " + spell.time)
+    print("Range:        " + spell.range)
+    print("Components:   " + spell.comp)
+    print("Duration:     " + spell.dur + "\n")
+    print("Description: \n" + spell.desc)
--- Spell.py
+++ Spell.py
@@ -1,6 +1,8 @@
 class Spell:
     "A spell is a discrete magical effect, a single shaping of the magical energies that suffuse the multiverse into a specific, limited expression. In casting a spell, a character carefully plucks at the invisible strands of raw magic suffusing the world, pins them in place in a particular pattern, sets them vibrating in a specific way, and then releases them to unleash the desired effect—in most cases, all in the span of seconds."
-    def __init__(self, level, dndClass, castingTime, spellRange, components, duration, desc):
+    def __init__(self, name, school, level, dndClass, castingTime, spellRange, components, duration, desc):
+        self.name = name
+        self.school = school
         self.dclass = dndClass
         self.level = level
         self.time = castingTime
